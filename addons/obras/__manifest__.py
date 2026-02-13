4bb143c2994180c3a9151d631282c75b0aea22b4
GuillermoSH <siciliahernandezguillermo@gmail.com>
2026-02-13 13:54:47 +0000

    :sparkles: Se añade la funcionalidad de crear reportes en obras

--- addons/obras/__manifest__.py
+++ addons/obras/__manifest__.py
@@ -7,7 +7,8 @@
     'depends': ['base'],  # Dependencias necesarias, en este caso solo 'base' que es el modulo principal de Odoo
     'data': [
         'security/ir.model.access.csv', # Archivo de seguridad para definir permisos de acceso al modulo
-        'views/obra_views.xml', # Localizacion de las vistas
+        'views/obra_views.xml',
+        'views/obra_reports.xml',
     ],
     'installable': True,
     'application': True, # Aparece en el menu de aplicaciones directamente
